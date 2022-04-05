c6e55130642146240159e7892f26c80151ae0971
logan1x <sharmakhushal78@gmail.com>
2022-04-05 22:30:03 +0530

    Refactor: minor styling changes

--- src/pages/watchLater/WatchLater.jsx
+++ src/pages/watchLater/WatchLater.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 import "./watchLater.css";
 import { useDataContext } from "../../context/dataContext";
@@ -16,17 +17,19 @@ export default function WatchLater() {
 
   return (
     <div>
-      <h1>Watch Later</h1>
+      <h1 className="watchLater-heading">Watch Later</h1>
       <div className="watchLater-wrapper">
         {watchLater &&
           watchLater.map(({ _id, title, creator, embedId }) => {
             return (
               <div key={_id} className="videoListing-card">
-                <img
-                  className="videoListing-card-image"
-                  src={`https://i.ytimg.com/vi/${embedId}/hqdefault.jpg`}
-                  alt=""
-                />
+                <Link to={`/videodetail/${_id}`}>
+                  <img
+                    className="videoListing-card-image"
+                    src={`https://i.ytimg.com/vi/${embedId}/hqdefault.jpg`}
+                    alt=""
+                  />
+                </Link>
                 <div>
                   <h2>{title}</h2>
                   <p>{creator}</p>
